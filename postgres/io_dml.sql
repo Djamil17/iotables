e5691f0f9fc6b608fecc2d89912430534e37638d
dl2774@columbia.edu <dl2774@columbia.edu>
2020-07-02 15:04:06 -0400

    deletions,clean-up

--- postgres/io_dml.sql
+++
@@ -1,4 +0,0 @@
-copy iotable from '/Users/djamillakhdarhamina/Desktop/iodata/iodata.csv' WITH (FORMAT csv);
-copy iocode_cw from '/Users/djamillakhdarhamina/Desktop/iodata/clean_iocode_cw.txt' WITH (FORMAT csv);
-copy iocode_vw from '/Users/djamillakhdarhamina/Desktop/iodata/clean_iocode_vw.txt' WITH (FORMAT csv);
-
--- postgres/query_recipes.sql
+++
@@ -1,6 +0,0 @@
--- so y= axij + xi1j1 +  xi2j2 ... xinjn in relational terms given the model has two basic cases
--- axij is select a(i) from iotable where name='j' so the amount of i that goes into j 
--- direct is just select select a(i) where name=i; e.g. :
-select farms from iotable where name='Farms';
---indirect is just select (i) where name=j; e.g. : 
-select Forestry_fishing_related_activities from iotable where name='Farms';
